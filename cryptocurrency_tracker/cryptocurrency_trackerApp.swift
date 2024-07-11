5c675c2a7f99b0a84143c45ff8c8ea09e627952b
mohamdan <engineerm86@gmail.com>
2024-07-11 17:30:19 +0300

    Initial Commit

---
+++ cryptocurrency_tracker/cryptocurrency_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptocurrency_trackerApp.swift
+//  cryptocurrency_tracker
+//
+//  Created by mohamdan on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct cryptocurrency_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
