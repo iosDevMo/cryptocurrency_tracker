8bee524e883377304bbe79f1869c4fc7ae38d37a
mohamdan <engineerm86@gmail.com>
2024-07-15 17:34:02 +0300

    [Claen] add documentation to currency formatter

--- cryptocurrency_tracker/Extensions/Double.swift
+++ cryptocurrency_tracker/Extensions/Double.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 extension Double {
-    
+    /// Converts a Double into a Currency with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// Convert 12.3456 to $12.3456
+    /// Convert 0.123456 to $0.123456
+    /// ```
     private var currencyFormatter6: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
@@ -21,6 +26,12 @@ extension Double {
         return formatter
     }
     
+    /// Converts a Double into a Currency  as a String with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// Convert 12.3456 to "$12.3456"
+    /// Convert 0.123456 to "$0.123456"
+    /// ```
     func asCurrencyWith6Decimals() -> String {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "$0.00"
